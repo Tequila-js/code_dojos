7785bea9255f06f353e40d704ed71dfddf8b75e6
Marco Antonio Dominguez Rueda <madmzr@gmail.com>
2017-01-12 14:13:45 -0600

    Create members components - SASS will be compiled using webpack - Added new value into configuration - Added member component

--- app/js/pages/members.js
+++ app/js/pages/members.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Axios from 'axios';
 
 import Loader from '../components/loader.js';
+import Member from '../components/member/member.js';
 
 const tequilaTeamMembers = 'https://api.github.com/orgs/Tequila-js/members';
 
@@ -29,12 +30,16 @@ class Members extends React.Component {
   render() {
     return (<section>
               <h1>Coding Dojo Members</h1>
-              <ul> {this.state.members.length? this.returnMembers(): <Loader/>} </ul>
+              {this.state.members.length? this.returnMembers(): <Loader/>}
             </section>)
   }
 
   returnMembers() {
-    return this.state.members.map((item, index) => (<li key={index}>{item.login}</li>));
+    return (<ul className="list-unstyled">
+                {this.state.members.map((item, index) => (<li key={index}>
+                                                              <Member name={item.login} url={item.html_url} avatar={item.avatar_url}/>
+                                                          </li>))}
+            </ul>)
   }
 
 }
