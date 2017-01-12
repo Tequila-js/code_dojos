1f1ec73a32d75337ef56b5605f06e9f38b9bbd47
Marco Antonio Dominguez Rueda <madmzr@gmail.com>
2017-01-12 12:48:54 -0600

    Create loader component - React component - SASS component

---
+++ app/js/components/loader.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+class Loader extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (<div className="loader"></div>)
+  }
+}
+
+export default Loader;
